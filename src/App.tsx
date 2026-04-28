9943e55bfea44c5a1eb8515b740581f324419825
Madi <khapezmadi@gmail.com>
2026-04-28 21:54:06 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Texter</h1>
+      <Button onClick={() => setCount(count + 1)}>Count: {count}</Button>
+    </div>
+  );
+};
+
+export default App;
